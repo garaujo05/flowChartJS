69c2ff1a211412c2b3b00bc4a15435488de80d85
Gabriel Meira Araújo <bielmagma@icloud.com>
2023-05-29 19:44:58 -0300

    Update newAnimateFlowChart.js

--- newAnimateFlowChart.js
+++ newAnimateFlowChart.js
@@ -1,11 +1,14 @@
-
 var CSVMap = new Map();
 
 var nodesID = new Map();
 
 var edgesID = new Map();
 
+var myAnimation = new Array();
+
 var submitCount = 0;
+
+var paused = true;
 
 var weights = {
     "s": 2,
@@ -16,67 +19,9 @@ var weights = {
     "A": 7
 };
 
-var svgPath = d3.select('body')
-.select('svg')
-.select('g')
-.select('edge')
-.select('g')
-.select('a')
-
-var svg2 = d3.select('body')
-.select('svg')
-.select('g')
-.selectAll('.node');
-
-svg2.each(function (p, j){
-
-    let selection = d3.select(this).select("g");
-
-    if(selection.empty() == false){
-        let title = d3.select(this).select("g").select("a").attr("xlink:title");
-        nodesID.set(title, this.id);
-    }
-
-});
-
-var svg = d3.select('body')
-    .select('svg')
-    .select('g')
-    .selectAll('.edge');
-
-svg.each(function (p, j){
-
-    let ID = d3.select(this).select('g').select("a").attr("xlink:title");
-    let temp = this.id;
-
-    let time = d3.select(this).selectAll('g');
-
-    time.each(function(p, j) {
-
-        if(this.id.includes('-',6) == true){
-            let text = d3.select(this).select('a').select('text');
-
-            if(text.attr('text-anchor') == 'middle'){
-                let trimmedText = text.text().trim();
-
-                // if(!weights.has(trimmedText[trimmedText.length-1])){
-                //     weights.set(trimmedText[trimmedText.length-1], );
-                // }
-
-                edgesID.set(ID, [temp, trimmedText]);
-                // console.log(trimmedText[trimmedText.length-1]);
-
-            }
-        }
-
-    });
-});
-
 function readCSVFile(){
 
     var files = document.querySelector('#file').files;
-
-    console.log(files);
 
     if(files.length > 0){
 
@@ -93,12 +38,11 @@ function readCSVFile(){
             var rowData = csvdata.split(';');
 
             loadCSV(rowData);
-            console.log(CSVMap.size);
 
-            let timeSpent = 500;
-
+            var circles = new Array(CSVMap.size);
+            
             for(let i = 1; i < CSVMap.size+1;i++){
-                // console.log(CSVMap.get(i));
+
                 if(CSVMap.get(i)[0].length == 1){
                     continue;
                 }
@@ -124,8 +68,7 @@ function readCSVFile(){
                 .attr('fill', 'lightslategray')
                 .attr('fill-opacity', '0.9');
 
-                // console.log(CSVMap.get(i)[1]);
-                timeSpent = (500 + 100*CSVMap.get(i)[1]);
+                circles[i-1] = new Circle(i, CSVMap.get(i)[1]/10);
 
                 for(let j = 0; j < CSVMap.get(i)[0].length-1; j++){
 
@@ -138,30 +81,78 @@ function readCSVFile(){
                     .select('path');
 
                     let totalLength = edge.node().getTotalLength();
-                    // Iniciar baseado no tempo do processo.
 
+                    // Iniciar baseado no tempo do processo.
                     let time = edgesID.get(CSVMap.get(i)[0][j] + " -> " + CSVMap.get(i)[0][j+1])[1];
-                    // console.log(time);
- 
+
                     let unit = weights[time[time.length-1]];
 
                     let value = parseInt(time.slice(0,time.length-1));
                 
                     let animationTime = value/(unit+value) + unit-1;
 
-                    setTimeout(moveCircle.bind(this, totalLength, edgesID.get(CSVMap.get(i)[0][j] + " -> " + CSVMap.get(i)[0][j+1])[0], i, animationTime), timeSpent); 
-                    // console.log(timeSpent);
-
-                    timeSpent += animationTime*1000+500;
+                    circles[i-1].incrementNodes(CSVMap.get(i)[0][j], CSVMap.get(i)[0][j+1]);
+                    circles[i-1].incrementTotalLength(totalLength);
+                    circles[i-1].incrementAnimationTime(animationTime);
+                    circles[i-1].setFinal(!(j < CSVMap.get(i)[0].length-2));
 
                 }
-                // console.log(timeSpent);
 
-                id = setTimeout(deleteCircle.bind(this, 'circle' + i), (timeSpent+500)); 
+                moveCircle(circles[i-1].totalLength[0], circles[i-1].startNodes[0], circles[i-1].endNodes[0], circles[i-1].reference, circles[i-1].animationTime[0],circles[i-1].final[0], 1, circles, i-1, circles[i-1].delay);
 
             }
         }
     }
+}
+
+function moveCircle(totalLength, startNode, endNode, circleID, animationTime, final, index, circles, start, delay){
+
+    let svgPath3 = d3.select('body')
+        .select('svg')
+        .select('g')
+        .select('#' + edgesID.get(startNode + ' -> ' + endNode)[0])
+        .select('g')
+        .select('a')
+        .select('path');
+    
+    let svg = d3.select('#circle' + circleID);
+    
+    // Create an object that gsap can animate
+    let val = { distance: 0 };
+    // Create a tween
+    let tempAnimation = gsap.to(val, {
+    // Animate from distance 0 to the total distance
+    distance: totalLength,
+    duration: animationTime,
+    paused: paused,
+    delay: delay,
+
+    // Function call on each frame of the animation
+    onUpdate: () => {
+
+        // Query a point at the new distance value
+        let point = svgPath3.node().getPointAtLength(val.distance);
+
+        svg.attr('cx', point.x).attr('cy', point.y);
+
+    },
+
+    onComplete: () => {
+
+        incrementOpacity(endNode);
+
+        if(final){
+            deleteCircle('circle' + circleID);
+        } else{
+            // Vai pro próximo círculo
+            moveCircle(circles[start].totalLength[index], circles[start].startNodes[index], circles[start].endNodes[index], circles[start].reference, circles[start].animationTime[index], circles[start].final[index], index+1, circles, start, 1);
+        }
+    }
+
+    });
+
+    myAnimation.push(tempAnimation);
+
 }
 
 function loadCSV(rowData){
@@ -175,10 +166,12 @@ function loadCSV(rowData){
     let multiplier = 0;
     let granularity = document.querySelector('#number').value;
     submitCount++;
-    console.log(granularity);
 
     if(submitCount > 1){
-        restartCircles(id);
+        restartCircles();
+    } else{
+        loadEdgesID();
+        loadNodesID();
     }
 
     for(let i = 4; i < rowData.length; i += 4){
@@ -205,19 +198,145 @@ function loadCSV(rowData){
             } else {
 
                 currentEnd = rowData[i+2];
-                // console.log(currentEnd);
 
                 multiplier = timeElapsed(firstEnd, currentEnd, granularity);
-                // console.log(multiplier);
 
                 caseReference = caseNumber;
                 count += 1;
-                // CSVMap.set(count, [rowData[i+1]]);
                 CSVMap.set(count, [[rowData[i+1]], multiplier]);
                 indexOfArray = 0;
                 valid = true;
             }
         }
+    }
+}
+
+function loadNodesID(){
+
+    var svg2 = d3.select('body')
+    .select('svg')
+    .select('g')
+    .selectAll('.node');
+
+    svg2.each(function (p, j){
+
+        let selection = d3.select(this).select("g");
+
+        if(selection.empty() == false){
+            let title = d3.select(this).select("g").select("a").attr("xlink:title");
+            nodesID.set(title, [this.id, 0]);
+        }
+
+    });
+
+}
+
+function loadEdgesID(){
+    var svg = d3.select('body')
+    .select('svg')
+    .select('g')
+    .selectAll('.edge');
+
+    svg.each(function (p, j){
+
+        let ID = d3.select(this).select('g').select("a").attr("xlink:title");
+        let temp = this.id;
+
+        let time = d3.select(this).selectAll('g');
+
+        time.each(function(p, j) {
+
+            if(this.id.includes('-',6) == true){
+                let text = d3.select(this).select('a').select('text');
+
+                if(text.attr('text-anchor') == 'middle'){
+                    let trimmedText = text.text().trim();
+
+                    edgesID.set(ID, [temp, trimmedText]);
+                }
+            }
+
+        });
+    });
+}
+
+function incrementOpacity(endNode){
+
+    if(nodesID.get(endNode)[1] == 0){
+
+        d3.select('#'+nodesID.get(endNode)[0])
+        .select('a')
+        .select('path')
+        .attr('fill', 'rgb(120,172,255)')
+        .attr('fill-opacity', '1%');
+
+    } else if(nodesID.get(endNode)[1] < 100){
+
+        d3.select('#'+nodesID.get(endNode)[0])
+        .select('a')
+        .select('path')
+        .attr('fill-opacity', nodesID.get(endNode)[1]+0.01 + '%');
+    }
+
+    nodesID.get(endNode)[1] += 1;
+
+}
+
+function restartOpacity(){
+    var svg = d3.select('body')
+    .select('svg')
+    .select('g')
+    .selectAll('.node');
+
+    svg.each(function (p, j){
+
+        let selection = d3.select(this).select("g");
+
+        if(selection.empty() == false){
+            d3.select(this).select("g").select("a").select('path').attr('fill', '#ffffff').attr('fill-opacity', '100%');
+        }
+
+    });
+}
+
+function restartCircles(){
+    var delCircle = d3.selectAll('circle').remove();
+
+    for(let i = 0; i < myAnimation.length; i++){
+        myAnimation[i].kill();
+        myAnimation[i] = null;
+    }
+
+    CSVMap = new Map();
+    myAnimation = new Array();
+    restartOpacity();
+
+    nodesID.forEach((value) => {
+        // console.log(value[1]);
+        value[1] = 0;
+
+    });
+
+}
+
+function deleteCircle(circleId){
+    var del = d3.select('#'+circleId).remove();
+}
+
+function pause(){
+    paused = true;
+
+    for(let i = 0; i < myAnimation.length; i++){
+        myAnimation[i].paused(true);
+    }
+}
+
+function unpause(){
+    if(paused){
+        for(let i = 0; i < myAnimation.length; i++){
+            myAnimation[i].paused(false);
+        }
+        paused = false;
     }
 }
 
@@ -228,61 +347,40 @@ function timeElapsed(start, end, granularity){
 
     const duration = currentEnd.getTime() - firstEnd.getTime();
 
+    if(granularity <= 0){
+        granularity = 1;
+    }
+
     return Math.floor(duration /(1000*60*60)/granularity);
 
 }
 
-function moveCircle(totalLength, edgeID, circleID, animationTime){
-
-    const svgPath3 = d3.select('body')
-        .select('svg')
-        .select('g')
-        .select('#' + edgeID)
-        .select('g')
-        .select('a')
-        .select('path');
-    
-    const svg = d3.select('#circle' + circleID);
-
-    // Create an object that gsap can animate
-    const val = { distance: 0 };
-    // Create a tween
-    gsap.to(val, {
-    // Animate from distance 0 to the total distance
-    distance: totalLength,
-    duration: animationTime,
-    // Function call on each frame of the animation
-    onUpdate: () => {
-
-        // // Query a point at the new distance value
-        const point = svgPath3.node().getPointAtLength(val.distance);
-
-        svg.attr('cx', point.x).attr('cy', point.y);
-    }
-    });
-
-}
-
-function restartCircles(id){
-    var delCircle = d3.selectAll('circle').remove();
-
-    CSVMap = new Map();
-
-    while(id--){
-        window.clearTimeout(id);
+class Circle{
+    constructor(i=0, delay){
+        this.totalLength = [];
+        this.startNodes = [];
+        this.endNodes = [];
+        this.reference = i;
+        this.animationTime = [];
+        this.final = [];
+        this.delay = delay;
     }
 
+    incrementNodes(startNode, endNode){
+        this.startNodes.push(startNode);
+        this.endNodes.push(endNode);
+    }
+
+    incrementTotalLength(totalLength){
+        this.totalLength.push(totalLength);
+    }
+
+    incrementAnimationTime(animationTime){
+        this.animationTime.push(animationTime);
+    }
+
+    setFinal(final){
+        this.final.push(final);
+    }
 
 }
-
-function deleteCircle(circleId){
-    var del = d3.select('#'+circleId).remove();
-}
-
-
-/*
-
- */
-
-
-
